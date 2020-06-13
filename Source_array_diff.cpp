acc435d729ab3545bf03fcd1e7c8faf0831fa3be
Rob <robert5guerra@gmail.com>
2020-06-13 16:14:45 -0700

    add diff of max and min elem

---
+++ Source_array_diff.cpp
@@ -0,0 +1,30 @@
+/*
+Difference of Max and Min Numbers in Array
+Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int differenceMaxMin(std::vector<int> arr) {
+	int minNum = *min_element(arr.begin(), arr.end()); // find min element
+	int maxNum = *max_element(arr.begin(), arr.end()); // find max element
+
+	return maxNum - minNum;
+}
+
+int main()
+{
+	vector<int> myVec = { 2, 5, 99, 25, -34, 18 };
+
+	int result = differenceMaxMin(myVec);
+
+	cout << result << endl;
+
+	system("pause");
+	return 0;
+}
