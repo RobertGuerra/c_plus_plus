5f906d66d800a38d3d67ec535c5aecc410fa510d
Rob <robert5guerra@gmail.com>
2020-06-13 17:28:33 -0700

    added to comments

--- Source_array_diff.cpp
+++ Source_array_diff.cpp
@@ -1,6 +1,7 @@
 /*
 Difference of Max and Min Numbers in the Array
-Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+Create a function that takes an array and returns the difference 
+between the biggest and smallest numbers using vectors.
 */
 #include <iostream>
 #include <string>
