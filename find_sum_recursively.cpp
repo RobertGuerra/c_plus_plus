1a642323faa92c2b0dc08be94baa15098cfd22b2
Rob <robert5guerra@gmail.com>
2020-06-16 13:32:02 -0700

    recursively finds sum of first n natural numbers

---
+++ find_sum_recursively.cpp
@@ -0,0 +1,32 @@
+/*
+
+Recursion: Sum
+Write a function that finds the sum of the first n natural numbers. Make your function recursive.
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int sum(int n) {
+	if (n < 0)
+		return 0;
+	else
+		return n + sum(n - 1);
+}
+
+
+int main()
+{
+	int num = 0;
+	cout << "Enter an number: ";
+	cin >> num;
+	
+	cout << "The sum of " << num << " and all the numbers before it is " << sum(num);
+
+	cout << endl << endl;
+
+	system("pause");
+	return 0;
+}
